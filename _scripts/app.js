b6f10fbfdb8ceccb999938007d11bd02676347b9
ishdeep <ishdeepsingh7@gmail.com>
2023-03-15 12:10:53 -0400

    created a XHR request successfully

--- _scripts/app.js
+++ _scripts/app.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const url = 'https://us-street.api.smartystreets.com/street-address?auth-id=161930695241786352&street=22%20Degroat%20Rd&city=Sandyston&state=NJ&zipcode=07827&candidates=10';
+
+const createRequest = function(url){
+    const httpRequest = new XMLHttpRequest(url);
+    httpRequest.addEventListener('readystatechange', (url) => {
+        if(httpRequest.readyState === 4){
+            console.log(httpRequest.responseText);
+        }
+    });
+    httpRequest.open('GET', url);
+    httpRequest.send();
+};
+
+createRequest(url);
