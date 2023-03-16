f480aca1b2a9ba55017cdfeb2e980906f08454a9
ishdeep <ishdeepsingh7@gmail.com>
2023-03-15 22:01:39 -0400

    added parksUrl in app.js

--- _scripts/app.js
+++ _scripts/app.js
@@ -1,16 +1,33 @@
 'use strict';
 
-const url = 'https://us-street.api.smartystreets.com/street-address?auth-id=161930695241786352&street=22%20Degroat%20Rd&city=Sandyston&state=NJ&zipcode=07827&candidates=10';
+const smartyUrl = 'https://us-street.api.smartystreets.com/street-address?auth-id=161930695241786352&street=22%20Degroat%20Rd&city=Sandyston&state=NJ&zipcode=07827&candidates=10';
+
+const parksUrl = 'https://developer.nps.gov/api/v1/parks?stateCode=CA&api_key=lagsDOc4O8JDhdD5qeEuqQfwad46Sh7YwMBJ0ED8';
+
+const updateUISuccess = function(data){
+    console.log(data);
+};
+
+const updateUIError = function(error){
+    console.log(error);
+};
+
+const responseMethod = function(httpRequest){
+    if(httpRequest.readyState === 4){
+        if(httpRequest.status === 200){
+            updateUISuccess(httpRequest.responseText);
+        } else {
+            updateUIError(httpRequest.status + ': ' + httpRequest.responseText);
+        }
+    }
+};
 
 const createRequest = function(url){
     const httpRequest = new XMLHttpRequest(url);
-    httpRequest.addEventListener('readystatechange', (url) => {
-        if(httpRequest.readyState === 4){
-            console.log(httpRequest.responseText);
-        }
-    });
+    httpRequest.addEventListener('readystatechange', (url) => responseMethod(httpRequest));
     httpRequest.open('GET', url);
     httpRequest.send();
 };
 
-createRequest(url);
+//createRequest(smartyUrl);
+createRequest(parksUrl);
